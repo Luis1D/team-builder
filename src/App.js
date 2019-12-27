3fa102f9f40e3b21bc2e39def21a74a3ecfbabd1
Luis De Anda <luisdeanda916@outlook.com>
2019-12-27 09:57:44 -0800

    container added to forms

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header';
-import Members from './components/Members'
-import MemberList from './components/MemberList'
+import Member from "./components/Member";
+import Form from "./components/Form";
 import './App.css';
 
 function App() {
+  // Sets up state for each member
+  const [members, setMembers] = useState([]);
+
+  // Submits a member to state
+  const submitMember = member => setMembers([...members, member]);
+
+  // Logic for updating a new member
+  const update = newMember =>
+    setMembers([...members.map(member => {
+        if (member.id === newMember.id) {
+          return newMember;
+        }
+        return member;
+      })
+    ]);
+
+  // Returns App component with everything 
   return (
     <div className="App">
-        <Header />
-        <Members />
-        <MemberList />
+
+      <Header />
+
+      <Form add={submitMember} />
+
+      {/* MAPS OVER MEMBERS AND CREATES A CARD */}
+      {members.map((member, i) => (
+        <Member member={member} key={i} update={update} />
+      ))}
+
     </div>
   );
 }
